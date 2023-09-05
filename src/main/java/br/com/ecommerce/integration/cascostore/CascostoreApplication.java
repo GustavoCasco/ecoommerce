381383eef50925ed6612898e6df8e5428c6d288e
Gustavo Casco <gugugacasco007@gmail.com>
2023-09-05 20:06:17 -0300

    feat: primeiro commit

---
+++ src/main/java/br/com/ecommerce/integration/cascostore/CascostoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.integration.cascostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CascostoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ecommerce/integration/cascostore/CascostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.integration.cascostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CascostoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
